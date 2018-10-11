07abd4dcddaafb2f93ad1154b782408582ef8327
Pak <Pak@admins-imac-69.lan>
2018-10-11 12:17:03 -0400

    cloudinary

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { cloudName, unsignedKey } from './api_key/cloud-key';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    pictures: []
+  }
+
+
+  componentDidMount() {
+
+    axios.get(`http://res.cloudinary.com/${cloudName}/image/list/hamster.json`)
+      .then( result => {
+        this.setState({
+          pictures: result.data.resources
+        });
+      })
+      .catch( err => {
+        console.log(err);
+      })
+
+  }
+
+  uploadWidget = () => {
+    
+    window.cloudinary.openUploadWidget({ 
+                                        cloud_name: cloudName, 
+                                        upload_preset: unsignedKey, 
+                                        tags:['hamster']}, (error, result) => {
+
+                                          if (error) {
+                                            console.log(error);
+                                          } else {
+                                            console.log(result);
+                                          }
+
+                                        });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.uploadWidget}>Add Image</button>  
+
+        {
+          this.state.pictures.map(data => {
+            return (
+              <div key={data.public_id}>
+                <img alt="" src={`http://res.cloudinary.com/${cloudName}/image/upload/${data.public_id}.jpg`} />
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
 }
 
 export default App;
+
